b22ce63c149aa0b26fdd1bc3d7a60bb4f1832278
Tom Claflin <thomas.claflin@mymail.champlain.edu>
2023-02-05 11:22:25 -0500

    week 3 dns servers

---
+++ sec335/week3/portscanner.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+host=$1
+port=$2
+
+
+echo "host, port"
+
+for i in $(seq 1 254); do
+	timeout .1 bash -c "echo >/dev/tcp/$host.$i/$port" 2>/dev/null &&
+		echo "$host.$i, $port"
+	done
